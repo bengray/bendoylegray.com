150e1af8ab3ba117dd21e446b5dad1d080b84115
Benjamin Gray <benjamingray@Benjamins-MacBook-Pro.local>
2019-08-16 22:26:16 -0400

    Changes picture of me.  Also updates the work section and about section

--- src/components/Main.js
+++ src/components/Main.js
@@ -17,7 +17,7 @@ class Main extends React.Component {
           <span className="image main"><img src={me} alt="" /></span>
           <p>Hi! I'm Ben Gray, welcome. I've been building software for the web for a decade and some change.</p>
           <p>I LOVE working within teams of cross-functional, passionate people.
-            I've been fortunate enough to have been on a variety of teams that have built everything from customized data collection and monitoring solutions for Georgia Power 
+            I've been fortunate enough to have been on a variety of teams that have built everything from customized data collection and monitoring solutions for Georgia Power
             to a full stack internal CMS for the writers and editors at CNN.  Through the years my passion remains the same: quickly producing quality software with teams of awesome people.</p>
           {close}
         </article>
@@ -26,7 +26,14 @@ class Main extends React.Component {
           <h2 className="major">Work</h2>
           <span className="image main"><img src={tool} alt="" /></span>
           <p>
-            I'm an experienced full stack JavaScript developer with 10+ years of professional experience. Primary expertise: JavaScript, ReactJS, Redux, Node.js, HTML, CSS, Docker, MongoDB, MySQL.
+            I'm an experienced full stack JavaScript developer with 10+ years of professional experience.
+          </p>
+          <p><span className="emphasis">Primary expertise</span>: JavaScript, ReactJS, Redux, Node.js, HTML, CSS, Docker, AWS, MongoDB.</p>
+          <p><span className="emphasis">Secondary expertise</span>: Angular, MySQL, PHP, Java, MS Exchange, Linux server administration.</p>
+
+          <h3>Cricket Wireless</h3>
+          <p>
+            Full Stack JavaScript engineer responsible for leading the CMS team through the full SDLC.  This CMS delivers content to the main Cricket Wireless consumer web portal as well a front-end for writer and editor use.  Responsible for developing, deploying and maintaining the entire stack, from AWS ECS & EC2 to writing front-end JavaScript modules complete with “Bring Your Own” styling in a UMD model.
           </p>
 
           <h3>CNN</h3>
@@ -51,7 +58,7 @@ Developed commercial web and e-commerce applications for multiple high-traffic w
 
           <h3>JMX Services</h3>
           <p>
-          Designed and implemented the online customer interface for transmission equipment condition assessment monitoring for Georgia Power. Utilized stored procedures, MSSQL database, HTML, Javascript, SSL certificates, AES encryption, SHA-2 hashing and CSS to achieve this goal. 
+          Designed and implemented the online customer interface for transmission equipment condition assessment monitoring for Georgia Power. Utilized stored procedures, MSSQL database, HTML, Javascript, SSL certificates, AES encryption, SHA-2 hashing and CSS to achieve this goal.
           </p>
           {close}
         </article>
@@ -59,6 +66,15 @@ Developed commercial web and e-commerce applications for multiple high-traffic w
         <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">About</h2>
           <span className="image main"><img src={stained_glass} alt="" /></span>
+          <p>
+            I have a few passions in life:
+            <ul>
+              <li>Family</li>
+              <li>Software</li>
+              <li>Nature</li>
+            </ul>
+            When I'm not behind a computer I'm likely on a trail somewhere, or fishing on a lake with my wife and kids.
+          </p>
           <p>
             Life is more than a resume.  Success is more than the balance in a bank account.  I strive to connect people through technologies, embracing the human element and wrangling the technical.
           </p>
